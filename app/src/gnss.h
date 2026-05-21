b0c7a3b51b27c3cb0fb4d596c73ce80a1426b37d
Will <wstrn66@gmail.com>
2026-05-22 02:42:26 +0800

    AI generated: show GNSS info on screen

--- app/src/gnss.h
+++ app/src/gnss.h
@@ -1,6 +1,21 @@
 #ifndef GNSS_H
 #define GNSS_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
+struct gnss_info {
+	bool has_fix;
+	int32_t lat_e6;
+	int32_t lon_e6;
+	uint8_t quality;
+	uint8_t satellites;
+	uint16_t hdop_x10;
+	uint16_t speed_kmh_x10;
+	uint16_t course_deg_x10;
+};
+
 void gnss_init(void);
+void gnss_get_info(struct gnss_info *info);
 
 #endif // GNSS_H
--- app/src/main_page.c
+++ app/src/main_page.c
@@ -7,8 +7,10 @@
 #include <zephyr/drivers/sensor.h>
 
 #include <math.h>
+#include <stdio.h>
 
 #include "battery.h"
+#include "gnss.h"
 
 #define LVGL_STACK_SIZE 2048
 #define LVGL_PRIORITY 5
@@ -23,12 +25,53 @@ struct lv_objs{
 		lv_obj_t* prs;
 		lv_obj_t* tmp;
 		lv_obj_t* alt;
+		lv_obj_t* gnss;
 };
 
 static uint32_t cnt = 0;
+
+static void format_coord(int32_t coord_e6, bool is_latitude, char *buf, size_t buf_size)
+{
+	char hemi;
+	int32_t abs_coord;
+	int32_t degrees;
+	int32_t fraction;
+
+	hemi = is_latitude ? 'N' : 'E';
+	abs_coord = coord_e6;
+
+	if (coord_e6 < 0) {
+		hemi = is_latitude ? 'S' : 'W';
+		abs_coord = -coord_e6;
+	}
+
+	degrees = abs_coord / 1000000;
+	fraction = abs_coord % 1000000;
+	snprintf(buf, buf_size, "%ld.%06ld%c",
+		 (long)degrees, (long)fraction, hemi);
+}
+
+static const char *hdop_grade(uint16_t hdop_x10)
+{
+	if (hdop_x10 == 0U) {
+		return "--";
+	}
+	if (hdop_x10 < 10U) {
+		return "Excellent";
+	}
+	if (hdop_x10 < 20U) {
+		return "Good";
+	}
+	if (hdop_x10 < 50U) {
+		return "Fair";
+	}
+	return "Poor";
+}
+
 void info_update(lv_timer_t * timer)
 {
 	struct lv_objs* objs = timer->user_data;
+	struct gnss_info gnss;
 	int32_t bat_level = battery_get_mv();
 	struct sensor_value pressure;
 	struct sensor_value temperature;
@@ -37,6 +80,10 @@ void info_update(lv_timer_t * timer)
 	uint8_t prs_buf[20];
 	uint8_t tmp_buf[18];
 	uint8_t alt_buf[20];
+	char lat_buf[20];
+	char lon_buf[20];
+	const char *hdop_text;
+	char gnss_buf[128];
 	sprintf(bat_buf, "%4dmv", bat_level);
 	sensor_sample_fetch(pressure_dev);
 	sensor_channel_get(pressure_dev, SENSOR_CHAN_PRESS, &pressure);
@@ -46,12 +93,36 @@ void info_update(lv_timer_t * timer)
 	sprintf(prs_buf, "%6d.%01dPa", pressure.val1, pressure.val2/100000);
 	sprintf(tmp_buf, "%2d.%02dC", temperature.val1, temperature.val2/10000);
 	sprintf(alt_buf, "%4d.%01dm", altitude/10, altitude%10);
+	gnss_get_info(&gnss);
+	hdop_text = hdop_grade(gnss.hdop_x10);
+
+	if (gnss.has_fix) {
+		format_coord(gnss.lat_e6, true, lat_buf, sizeof(lat_buf));
+		format_coord(gnss.lon_e6, false, lon_buf, sizeof(lon_buf));
+		snprintf(gnss_buf, sizeof(gnss_buf),
+			 "Lat:%s\nLon:%s\nHDOP:%u.%u %s\nQ:%u\nSat:%u\nSpd:%u.%ukm/h\nCog:%u.%udeg",
+			 lat_buf, lon_buf,
+			 gnss.hdop_x10 / 10U, gnss.hdop_x10 % 10U, hdop_text,
+ 			gnss.quality,
+			 gnss.satellites,
+			 gnss.speed_kmh_x10 / 10U, gnss.speed_kmh_x10 % 10U,
+			 gnss.course_deg_x10 / 10U, gnss.course_deg_x10 % 10U);
+	} else {
+		snprintf(gnss_buf, sizeof(gnss_buf),
+			 "Lat:--\nLon:--\nHDOP:%u.%u %s\nQ:%u\nSat:%u\nSpd:%u.%ukm/h\nCog:%u.%udeg",
+			 gnss.hdop_x10 / 10U, gnss.hdop_x10 % 10U, hdop_text,
+			gnss.quality, 
+			 gnss.satellites,
+			 gnss.speed_kmh_x10 / 10U, gnss.speed_kmh_x10 % 10U,
+			 gnss.course_deg_x10 / 10U, gnss.course_deg_x10 % 10U);
+	}
 
 	k_mutex_lock(&lvgl_mutex, K_FOREVER);
 	lv_label_set_text(objs->bat, bat_buf);
 	lv_label_set_text(objs->prs, prs_buf);
 	lv_label_set_text(objs->tmp, tmp_buf);
 	lv_label_set_text(objs->alt, alt_buf);
+	lv_label_set_text(objs->gnss, gnss_buf);
 
 	k_mutex_unlock(&lvgl_mutex);
 }
@@ -62,6 +133,7 @@ void lvgl_entry_point(void *, void *, void *)
 	lv_obj_t * prs_label;
 	lv_obj_t * tmp_label;
 	lv_obj_t * alt_label;
+	lv_obj_t * gnss_label;
 	lv_timer_t * info_update_timer;
 	struct lv_objs main_page_objs;
 
@@ -78,6 +150,9 @@ void lvgl_entry_point(void *, void *, void *)
 	alt_label = lv_label_create(lv_scr_act());
 	lv_label_set_text(alt_label, "Altitude:");
 	lv_obj_align(alt_label, LV_ALIGN_TOP_LEFT, 0, 68);
+	gnss_label = lv_label_create(lv_scr_act());
+	lv_label_set_text(gnss_label, "GNSS:");
+	lv_obj_align(gnss_label, LV_ALIGN_TOP_LEFT, 0, 84);
 
 	main_page_objs.bat = lv_label_create(lv_scr_act());
 	lv_label_set_text(main_page_objs.bat, "0000mv");
@@ -91,6 +166,11 @@ void lvgl_entry_point(void *, void *, void *)
 	main_page_objs.alt = lv_label_create(lv_scr_act());
 	lv_label_set_text(main_page_objs.alt, "0000m");
 	lv_obj_align(main_page_objs.alt, LV_ALIGN_TOP_LEFT, 72, 68);
+	main_page_objs.gnss = lv_label_create(lv_scr_act());
+	lv_label_set_long_mode(main_page_objs.gnss, LV_LABEL_LONG_WRAP);
+	lv_obj_set_width(main_page_objs.gnss, 128);
+	lv_label_set_text(main_page_objs.gnss, "Lat:--\nLon:--\nHDOP:0.0 --\nQ:0\nSat:0\nSpd:0.0km/h\nCog:0.0deg");
+	lv_obj_align(main_page_objs.gnss, LV_ALIGN_TOP_LEFT, 0, 94);
 
 	/*Create a buffer for the canvas*/
     #define CANVAS_WIDTH  128
